8197b3df16f8736cd43d265865b381093a06562d
Christina Phan <88528865+christinaphan@users.noreply.github.com>
2023-09-06 10:10:44 -0700

    Add snippets

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -91,11 +91,19 @@ local plugins = {
 			"hrsh7th/cmp-nvim-lsp",
 			"hrsh7th/cmp-buffer",
 			"hrsh7th/cmp-path",
-			"hrsh7th/cmp-cmdline",
+			"saadparwaiz1/cmp_luasnip",
 		},
 		config = function()
 			require("user.cmp")
 		end,
+	},
+	{
+		"L3MON4D3/LuaSnip",
+		-- follow latest release.
+		version = "2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+		-- install jsregexp (optional!).
+		build = "make install_jsregexp",
+		dependencies = { "rafamadriz/friendly-snippets" },
 	},
 	{
 		"nvim-treesitter/nvim-treesitter",
