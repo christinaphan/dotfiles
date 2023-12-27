970eb7006f1743d2a8a86fe74643f6e8379d83d3
Christina Phan <88528865+christinaphan@users.noreply.github.com>
2023-12-27 14:56:17 -0800

    Fix nvim setup

--- .config/nvim/lua/user/hardtime.lua
+++
@@ -1,9 +0,0 @@
-local hardtime_status, hardtime = pcall(require, "hardtime")
-if not hardtime_status then
-	return
-end
-
-hardtime.setup({
-	disable_mouse = false,
-	disabled_filetypes = { "NvimTree", "mason", "help", "lazy", "lspinfo" },
-})
