2b37693f34846c06a9841cb12db2971dc3e522c1
Christina Phan <88528865+christinaphan@users.noreply.github.com>
2025-02-26 11:27:45 -0800

    set commit to last known good tse vimtex

--- .config/nvim/lua/user/plugins/vimtex.lua
+++ .config/nvim/lua/user/plugins/vimtex.lua
@@ -3,8 +3,7 @@ return {
   ft = "tex",
   init = function()
     vim.g.tex_flavor = "latex"
-    vim.g.vimtex_view_method = "skim"
-    vim.g.vimtex_view_skim_sy = 1
-    vim.g.vimtex_view_skim_activate = 1
+    vim.g.vimtex_view_method = "zathura"
   end,
+  commit = "fcbaf66"
 }
