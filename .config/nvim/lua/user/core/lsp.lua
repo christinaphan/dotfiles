7b76cddd40e9d4086381fc2332028513bdb81c52
Christina Phan <88528865+christinaphan@users.noreply.github.com>
2025-07-26 12:06:33 -0700

    remove lsp autocompletion

--- .config/nvim/lua/user/core/lsp.lua
+++ .config/nvim/lua/user/core/lsp.lua
@@ -11,16 +11,6 @@ vim.diagnostic.config({
   },
 })
 
--- enable lsp autocompletion
-vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(ev)
-    local client = vim.lsp.get_client_by_id(ev.data.client_id)
-    if client:supports_method("textDocument/completion") then
-      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-    end
-  end,
-})
-
 -- lsp keymappings
 vim.api.nvim_create_autocmd("LspAttach", {
   group = vim.api.nvim_create_augroup("UserLspConfig", {}),
