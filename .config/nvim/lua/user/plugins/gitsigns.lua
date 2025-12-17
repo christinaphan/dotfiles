5a727e6391280b016cbbaf0a96848d776084d1c4
Christina Phan <88528865+christinaphan@users.noreply.github.com>
2025-12-17 10:34:56 -0800

    update treesitter

--- .config/nvim/lua/user/plugins/gitsigns.lua
+++ .config/nvim/lua/user/plugins/gitsigns.lua
@@ -3,15 +3,25 @@ return {
   event = "VeryLazy",
   opts = {
     signs = {
-      add = { text = "+", },
-      change = { text = "+", },
-      delete = { text = "_", },
-      topdelete = { text = "‾", },
-      changedelete = { text = "~", },
+      add = { text = "+" },
+      change = { text = "+" },
+      delete = { text = "_" },
+      topdelete = { text = "‾" },
+      changedelete = { text = "~" },
     },
     signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-    numhl = false,     -- Toggle with `:Gitsigns toggle_numhl`
-    linehl = false,    -- Toggle with `:Gitsigns toggle_linehl`
+    numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+    linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
     word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
   },
+  -- stylua: ignore
+  keys = {
+    { "<leader>gl", ":Gitsign setqflist('all')<CR>" },
+    { "<leader>gs", function() require("gitsigns").stage_hunk() end },
+    { "<leader>gs", function() require("gitsigns").stage_hunk({ vim.fn.line('.'), vim.fn.line('v') }) end, mode="v" },
+    { "<leader>gr", function() require("gitsigns").reset_hunk() end },
+    { "<leader>gr", function() require("gitsigns").reset_hunk({ vim.fn.line('.'), vim.fn.line('v') }) end, mode="v" },
+    { "<leader>gp", function() require("gitsigns").preview_hunk() end },
+  }
+,
 }
